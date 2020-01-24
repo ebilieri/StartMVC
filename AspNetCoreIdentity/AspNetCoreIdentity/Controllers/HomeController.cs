553d77b5e96726d3765875cec2a98513775fa600
Emerson Bilieri Claudelino <ebilieri@gmail.com>
2020-01-23 22:53:19 -0300

    adicionado Identity na aplicação

---
+++ AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreIdentity.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin, Gestor")]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
